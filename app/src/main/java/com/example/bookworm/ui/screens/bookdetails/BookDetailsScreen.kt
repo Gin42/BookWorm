a5069ba852ac490b3fb224d859ff15828d8e3f88
Ginevra Bartolini <ginevra.bartolini@studio.unibo.it>
2025-12-27 11:11:24 +0100

    Fixing book details screen: changed book journey and diary entry style.

--- app/src/main/java/com/example/bookworm/ui/screens/bookdetails/BookDetailsScreen.kt
+++ app/src/main/java/com/example/bookworm/ui/screens/bookdetails/BookDetailsScreen.kt
@@ -2,16 +2,21 @@ package com.example.bookworm.ui.screens.bookdetails
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -20,6 +25,9 @@ import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.ArrowDropUp
 import androidx.compose.material.icons.outlined.Book
 import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -124,53 +132,7 @@ fun BookDetailsScreen(
 
             //book status
             item {
-                ListItem(
-                    modifier = Modifier
-                        .border(1.dp, Color.Transparent)
-                        .clip(MaterialTheme.shapes.medium),
-                    headlineContent = {
-                        Text(
-                            state.readingStatus.name.replace('_', ' ').lowercase()
-                                .replaceFirstChar { it.titlecase() },
-                            style = MaterialTheme.typography.titleMedium
-                        )
-                    },
-                    trailingContent = {
-                        IconButton(
-                            onClick = { actions.toggleStatusExpanded(!state.statusExpanded) },
-                        ) {
-                            Icon(
-                                imageVector = if (state.statusExpanded) {
-                                    Icons.Filled.ArrowDropUp
-                                } else {
-                                    Icons.Filled.ArrowDropDown
-                                },
-                                contentDescription = "Status options"
-                            )
-                        }
-                        DropdownMenu(
-                            expanded = state.statusExpanded,
-                            onDismissRequest = { actions.toggleStatusExpanded(false) }
-                        ) {
-                            ReadingStatus.entries.forEach { status ->
-                                DropdownMenuItem(
-                                    onClick = {
-                                        actions.setReadingStatus(status)
-                                        actions.toggleStatusExpanded(false)
-                                    },
-                                    text = {
-                                        Text(
-                                            status.name.replace('_', ' ').lowercase()
-                                                .replaceFirstChar { it.titlecase() })
-                                    },
-                                )
-                            }
-                        }
-                    },
-                    colors = ListItemDefaults.colors(
-                        containerColor = MaterialTheme.colorScheme.surfaceContainer
-                    )
-                )
+                StatusSelection(state, actions)
             }
 
             //progress bar
@@ -206,119 +168,170 @@ fun BookDetailsScreen(
     }
 }
 
-
 @Composable
-fun BookJourney(index: Int, state: BookDetailsState, actions: BookDetailsAction) {
-    Column (
+fun StatusSelection(state: BookDetailsState, actions: BookDetailsAction) {
+    ListItem(
         modifier = Modifier
-            .clickable { actions.toggleJourneyEntry(index) },
-    ) {
-        ListItem(
-            headlineContent = {
-                Text(
-                    "dd/mm/aaaa - dd/mm/aaaa",
-                    style = MaterialTheme.typography.titleMedium
+            .border(1.dp, Color.Transparent)
+            .clip(MaterialTheme.shapes.medium),
+        headlineContent = {
+            Text(
+                state.readingStatus.name.replace('_', ' ').lowercase()
+                    .replaceFirstChar { it.titlecase() },
+                style = MaterialTheme.typography.titleMedium
+            )
+        },
+        trailingContent = {
+            IconButton(
+                onClick = { actions.toggleStatusExpanded(!state.statusExpanded) },
+            ) {
+                Icon(
+                    imageVector = if (state.statusExpanded) {
+                        Icons.Filled.ArrowDropUp
+                    } else {
+                        Icons.Filled.ArrowDropDown
+                    },
+                    contentDescription = "Status options"
                 )
-            },
-            supportingContent = {
-                Text("N. entries: ##")
-            },
-            trailingContent = {
-                IconButton(
-                    onClick = { actions.toggleJourneyEntry(index) },
-                ) {
-                    Icon(
-                        imageVector = if (state.journeyExpanded[index]) {
-                            Icons.Filled.ArrowDropUp
-                        } else {
-                            Icons.Filled.ArrowDropDown
+            }
+            DropdownMenu(
+                expanded = state.statusExpanded,
+                onDismissRequest = { actions.toggleStatusExpanded(false) }
+            ) {
+                ReadingStatus.entries.forEach { status ->
+                    DropdownMenuItem(
+                        onClick = {
+                            actions.setReadingStatus(status)
+                            actions.toggleStatusExpanded(false)
                         },
-                        contentDescription = "Status options"
+                        text = {
+                            Text(
+                                status.name.replace('_', ' ').lowercase()
+                                    .replaceFirstChar { it.titlecase() })
+                        },
                     )
                 }
             }
+        },
+        colors = ListItemDefaults.colors(
+            containerColor = MaterialTheme.colorScheme.surfaceContainer
         )
+    )
+}
 
-        AnimatedVisibility(visible = state.journeyExpanded[index]) {
-            Column {
-                repeat(NUMBER_OF_ENTRIES) { entryIndex ->
-                    DiaryEntry(entryIndex, state, actions)
-                    if(entryIndex< NUMBER_OF_ENTRIES-1) {
-                        HorizontalDivider(
-                            modifier = Modifier.padding(bottom = 8.dp),
-                            thickness = 2.dp
+@Composable
+fun BookJourney(index: Int, state: BookDetailsState, actions: BookDetailsAction) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 8.dp)
+            .clickable { actions.toggleJourneyEntry(index) }
+            .border(1.dp, Color.Transparent)
+            .clip(MaterialTheme.shapes.medium)
+            .background(MaterialTheme.colorScheme.surfaceContainer),
+    ) {
+        Column {
+            ListItem(
+                headlineContent = {
+                    Text(
+                        "dd/mm/aaaa - dd/mm/aaaa",
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                },
+                supportingContent = {
+                    Text("N. entries: ##")
+                },
+                trailingContent = {
+                    IconButton(
+                        onClick = { actions.toggleJourneyEntry(index) },
+                    ) {
+                        Icon(
+                            imageVector = if (state.journeyExpanded[index]) {
+                                Icons.Filled.ArrowDropUp
+                            } else {
+                                Icons.Filled.ArrowDropDown
+                            },
+                            contentDescription = "Status options"
                         )
+                    }
+                },
+                colors = ListItemDefaults.colors(
+                    containerColor = MaterialTheme.colorScheme.surfaceContainer
+                )
+            )
+
+            AnimatedVisibility(visible = state.journeyExpanded[index]) {
+                Column {
+                    repeat(NUMBER_OF_ENTRIES) { entryIndex ->
+                        DiaryEntry(entryIndex, state, actions)
+                        if (entryIndex < NUMBER_OF_ENTRIES - 1) {
+                            HorizontalDivider(thickness = 2.dp)
+                        }
                     }
                 }
             }
-
-        }
-        if(index< NUMBER_OF_ENTRIES-1) {
-            HorizontalDivider(
-                modifier = Modifier.padding(bottom = 8.dp),
-                thickness = 2.dp
-            )
         }
     }
 }
-
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DiaryEntry(index: Int, state: BookDetailsState, actions: BookDetailsAction) {
-
-    val sheetState = rememberModalBottomSheetState(
-        skipPartiallyExpanded = false,
-    )
-
-    ListItem(
+    Box(
         modifier = Modifier
-            .clickable {
-                actions.openEntry(index, true)
-            },
-        headlineContent = {
-            Text(
-                "gg/mm/aaaa",
-                style = MaterialTheme.typography.bodyLarge
-            )
-        },
-        supportingContent = {
-            Text(
-                "Sit non officiis aliquam ipsa corporis quidem a id. Iste enim quidem animi aliquam eius. Error porro et suscipit quasi nostrum.",
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis,
-                style = MaterialTheme.typography.bodyMedium
-            )
-        }
-    )
-
-    if (state.entryExpanded[index]) {
-        ModalBottomSheet(
+            .fillMaxWidth()
+    ) {
+        ListItem(
             modifier = Modifier
-                .fillMaxHeight()
-                .fillMaxWidth(),
-            sheetState = sheetState,
-            onDismissRequest = { actions.openEntry(index, false) }
-        ) {
-            Column(
-                modifier = Modifier.padding(15.dp)
-            ) {
+                .clickable {
+                    actions.openEntry(index, true)
+                },
+            headlineContent = {
                 Text(
                     "gg/mm/aaaa",
-                    style = MaterialTheme.typography.headlineSmall,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(8.dp)
+                    style = MaterialTheme.typography.bodyLarge
                 )
-                Text(
-                    "Pages read: 250",
-                    style = MaterialTheme.typography.bodyLarge,
-                )
+            },
+            supportingContent = {
                 Text(
                     "Sit non officiis aliquam ipsa corporis quidem a id. Iste enim quidem animi aliquam eius. Error porro et suscipit quasi nostrum.",
-                    style = MaterialTheme.typography.bodyMedium,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    style = MaterialTheme.typography.bodyMedium
                 )
+            },
+            colors = ListItemDefaults.colors(
+                containerColor = MaterialTheme.colorScheme.surfaceContainer
+            )
+        )
+
+        if (state.entryExpanded[index]) {
+            ModalBottomSheet(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth(),
+                sheetState = rememberModalBottomSheetState(
+                    skipPartiallyExpanded = false,
+                ),
+                onDismissRequest = { actions.openEntry(index, false) }
+            ) {
+                Column(modifier = Modifier.padding(15.dp)) {
+                    Text(
+                        "gg/mm/aaaa",
+                        style = MaterialTheme.typography.headlineSmall,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(8.dp)
+                    )
+                    Text(
+                        "Pages read: 250",
+                        style = MaterialTheme.typography.bodyLarge,
+                    )
+                    Text(
+                        "Sit non officiis aliquam ipsa corporis quidem a id. Iste enim quidem animi aliquam eius. Error porro et suscipit quasi nostrum.",
+                        style = MaterialTheme.typography.bodyMedium,
+                    )
+                }
             }
         }
     }
 }
-
