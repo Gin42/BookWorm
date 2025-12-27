19404b7934996a68e190a7bc8b19975481f7b0e8
Ginevra Bartolini <ginevra.bartolini@studio.unibo.it>
2025-12-27 15:18:55 +0100

    Fixing library screen: created library composable.

---
+++ app/src/main/java/com/example/bookworm/ui/composables/Library.kt
@@ -0,0 +1,69 @@
+package com.example.bookworm.ui.composables
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Library(contentPadding: PaddingValues, navController: NavController, favourites: Boolean = false) {
+
+    /*SearchBar(
+        inputField = /*TODO*/,
+        expanded = expanded,
+        onExpandedChange = { expanded = it },
+    ) {
+    }*/
+
+    BookList(contentPadding, navController)
+}
+
+
+@Composable
+fun BookList(
+    contentPadding: PaddingValues,
+    navController: NavController
+    /*Gli si passano i libri che deve mostrare*/
+) {
+    val books = (1..20).map { "Title" }
+
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(8.dp),
+        modifier = Modifier.padding(contentPadding)
+    ) {
+        items(books) { item -> BookItem(item, navController) }
+    }
+}
